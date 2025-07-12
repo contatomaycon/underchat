4a7f9e1794323b6a6601d576bf41566795e506a7
Maycon Douglas <contatomaycon@gmail.com>
2025-07-12 20:17:48 -0300

    Adiciona modelos de usuário e suas relações ao sistema, incluindo user, userAddress, userDocument, userDocumentType, userInfo, userStatus e userType

---
+++ packages/core/models/user/userDocument.model.ts
@@ -0,0 +1,29 @@
+import { pgTable, timestamp, varchar, smallint } from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm';
+import { userDocumentType } from '@core/models';
+
+export const userDocument = pgTable('user_document', {
+  user_document_id: smallint()
+    .primaryKey()
+    .generatedByDefaultAsIdentity()
+    .notNull(),
+  user_document_type_id: smallint()
+    .references(() => userDocumentType.user_document_type_id)
+    .notNull(),
+  document: varchar({ length: 20 }).notNull(),
+  created_at: timestamp({
+    mode: 'string',
+    withTimezone: true,
+  }).defaultNow(),
+  updated_at: timestamp({
+    mode: 'string',
+    withTimezone: true,
+  }).defaultNow(),
+});
+
+export const userDocumentRelations = relations(userDocument, ({ one }) => ({
+  udt: one(userDocumentType, {
+    fields: [userDocument.user_document_type_id],
+    references: [userDocumentType.user_document_type_id],
+  }),
+}));
