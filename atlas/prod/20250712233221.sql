fc337fd56fb0881c9573840e8c6d36b9b6b22101
Maycon Douglas <contatomaycon@gmail.com>
2025-07-12 20:41:21 -0300

    Adiciona inserções para tipos de usuário, status de usuário e tipos de documentos, incluindo novos arquivos SQL para gerenciamento de dados.

--- atlas/prod/20250712233221.sql
+++ atlas/prod/20250712233221.sql
@@ -1,3 +1,3 @@
-INSERT INTO "user_type" ("name") VALUES ('Administrador');
-INSERT INTO "user_type" ("name") VALUES ('Cliente');
-INSERT INTO "user_type" ("name") VALUES ('Atendente');
+INSERT INTO "user_type" ("user_type_id", "name") VALUES (1, 'Administrador');
+INSERT INTO "user_type" ("user_type_id", "name") VALUES (2, 'Cliente');
+INSERT INTO "user_type" ("user_type_id", "name") VALUES (3, 'Atendente');
