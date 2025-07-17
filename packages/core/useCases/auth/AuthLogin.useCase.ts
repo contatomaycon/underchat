8081f953aa15552226e63de092e657df5b703a9e
Maycon Douglas <contatomaycon@gmail.com>
2025-07-17 12:11:14 -0300

    feat(auth): refatora lógica de autenticação e resposta de login, incluindo novos campos no retorno

--- packages/core/useCases/auth/AuthLogin.useCase.ts
+++ packages/core/useCases/auth/AuthLogin.useCase.ts
@@ -2,12 +2,46 @@ import { AuthService } from '@core/services/auth.service';
 import { injectable } from 'tsyringe';
 import { AuthLoginResponse } from '@core/schema/auth/login/response.schema';
 import { AuthLoginRequest } from '@core/schema/auth/login/request.schema';
+import { FastifyReply } from 'fastify';
+import { TFunction } from 'i18next';
+import { ERouteModule } from '@core/common/enums/ERouteModule';
+import { generalEnvironment } from '@core/config/environments';
 
 @injectable()
 export class AuthLoginUseCase {
   constructor(private readonly authService: AuthService) {}
 
-  async execute(input: AuthLoginRequest): Promise<AuthLoginResponse | null> {
-    return this.authService.authenticate(input.login, input.password);
+  async execute(
+    t: TFunction<'translation', undefined>,
+    reply: FastifyReply,
+    module: ERouteModule,
+    input: AuthLoginRequest
+  ): Promise<AuthLoginResponse | null> {
+    const result = await this.authService.authenticate(
+      input.login,
+      input.password
+    );
+
+    if (!result) {
+      throw new Error(t('login_invalid'));
+    }
+
+    const token = await reply.jwtSign(
+      {
+        user_id: result.user_id,
+        module,
+      },
+      {
+        sign: {
+          expiresIn: generalEnvironment.jwtSecretExpiresIn,
+          key: generalEnvironment.jwtSecret,
+        },
+      }
+    );
+
+    return {
+      user: result,
+      token,
+    };
   }
 }
