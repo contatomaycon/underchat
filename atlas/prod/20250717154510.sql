f56b767f1787ffab52b392f37597280b2a3df95c
Maycon Douglas <contatomaycon@gmail.com>
2025-07-17 12:52:47 -0300

    feat(database): atualiza e adiciona novos dados de usuários e permissões no banco de dados

---
+++ atlas/prod/20250717154510.sql
@@ -0,0 +1,8 @@
+INSERT INTO "permission_role_action" ("permission_role_action_id", "permission_action_id", "permission_role_id") VALUES (1, 1, 1);
+INSERT INTO "permission_role_action" ("permission_role_action_id", "permission_action_id", "permission_role_id") VALUES (2, 2, 1);
+INSERT INTO "permission_role_action" ("permission_role_action_id", "permission_action_id", "permission_role_id") VALUES (3, 3, 1);
+INSERT INTO "permission_role_action" ("permission_role_action_id", "permission_action_id", "permission_role_id") VALUES (4, 4, 1);
+INSERT INTO "permission_role_action" ("permission_role_action_id", "permission_action_id", "permission_role_id") VALUES (5, 5, 1);
+INSERT INTO "permission_role_action" ("permission_role_action_id", "permission_action_id", "permission_role_id") VALUES (6, 6, 1);
+INSERT INTO "permission_role_action" ("permission_role_action_id", "permission_action_id", "permission_role_id") VALUES (7, 7, 1);
+INSERT INTO "permission_role_action" ("permission_role_action_id", "permission_action_id", "permission_role_id") VALUES (8, 8, 1);
--- packages/core/common/enums/EPermissions/user.ts
+++ packages/core/common/enums/EPermissions/user.ts
@@ -1,3 +1,6 @@
 export enum EUserPermissions {
   user_view = 'user_view',
+  user_create = 'user_create',
+  user_update = 'user_update',
+  user_delete = 'user_delete',
 }
