8230d23d3cbdc93bdc281d1c44e7b6ef5a84d151
Maycon Douglas <contatomaycon@gmail.com>
2025-07-17 13:17:34 -0300

    feat(database): corrige status do servidor e atualiza hash do arquivo de soma

--- atlas/prod/20250717142545.sql
+++ atlas/prod/20250717142545.sql
@@ -1,6 +1,5 @@
 INSERT INTO "server_status" ("server_status_id", "status") VALUES (1, 'new');
-INSERT INTO "server_status" ("server_status_id", "status") VALUES (2, 'install');
-INSERT INTO "server_status" ("server_status_id", "status") VALUES (3, 'installing');
-INSERT INTO "server_status" ("server_status_id", "status") VALUES (4, 'online');
-INSERT INTO "server_status" ("server_status_id", "status") VALUES (5, 'error');
-INSERT INTO "server_status" ("server_status_id", "status") VALUES (6, 'offline');
+INSERT INTO "server_status" ("server_status_id", "status") VALUES (2, 'installing');
+INSERT INTO "server_status" ("server_status_id", "status") VALUES (3, 'online');
+INSERT INTO "server_status" ("server_status_id", "status") VALUES (4, 'error');
+INSERT INTO "server_status" ("server_status_id", "status") VALUES (5, 'offline');
