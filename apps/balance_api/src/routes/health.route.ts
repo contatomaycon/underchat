a75ded94ce96850a8eb7c72c037bf0679e1605e5
Maycon Douglas <contatomaycon@gmail.com>
2025-07-19 13:51:28 -0300

    feat: remove verificação de permissões no endpoint de saúde

--- apps/balance_api/src/routes/health.route.ts
+++ apps/balance_api/src/routes/health.route.ts
@@ -2,7 +2,6 @@ import HealthController from '@/controllers/health';
 import { FastifyInstance } from 'fastify';
 import { container } from 'tsyringe';
 import { healthCheckSchema } from '@core/schema/health';
-import { healthViewPermissions } from '@/permissions/health.permissions';
 
 export default async function healthRoutes(server: FastifyInstance) {
   const healthController = container.resolve(HealthController);
@@ -10,9 +9,5 @@ export default async function healthRoutes(server: FastifyInstance) {
   server.get('/health/check', {
     schema: healthCheckSchema,
     handler: healthController.view,
-    preHandler: [
-      (request, reply) =>
-        server.authenticateKeyApi(request, reply, healthViewPermissions),
-    ],
   });
 }
