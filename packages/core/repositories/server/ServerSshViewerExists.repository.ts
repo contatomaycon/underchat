7fb389cdd4f29b164565ab27a22d6c82e1caecf2
Maycon Douglas <contatomaycon@gmail.com>
2025-07-25 13:46:37 -0300

    feat(server): aprimorar verificação de servidor SSH por IP com junção e validação de exclusão

--- packages/core/repositories/server/ServerSshViewerExists.repository.ts
+++ packages/core/repositories/server/ServerSshViewerExists.repository.ts
@@ -1,8 +1,8 @@
 import * as schema from '@core/models';
-import { serverSsh } from '@core/models';
+import { server, serverSsh } from '@core/models';
 import { NodePgDatabase } from 'drizzle-orm/node-postgres';
 import { inject, injectable } from 'tsyringe';
-import { and, count, eq } from 'drizzle-orm';
+import { and, count, eq, isNull } from 'drizzle-orm';
 
 @injectable()
 export class ServerSshViewerExistsRepository {
@@ -16,7 +16,8 @@ export class ServerSshViewerExistsRepository {
         total: count(),
       })
       .from(serverSsh)
-      .where(and(eq(serverSsh.ssh_ip, ip)))
+      .innerJoin(server, eq(server.server_id, serverSsh.server_id))
+      .where(and(eq(serverSsh.ssh_ip, ip), isNull(server.deleted_at)))
       .execute();
 
     if (!result.length) {
