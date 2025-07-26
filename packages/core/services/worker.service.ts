6053e97a07ddc5a83df0ab3741f392298e4de469
Maycon Douglas <contatomaycon@gmail.com>
2025-07-26 09:53:03 -0300

    feat: implementar gerenciamento de trabalhadores com Docker e suporte a múltiplas imagens

--- packages/core/services/worker.service.ts
+++ packages/core/services/worker.service.ts
@@ -1,6 +1,58 @@
 import { injectable } from 'tsyringe';
+import Docker from 'dockerode';
+import { TFunction } from 'i18next';
+import { EWorkerImage } from '@core/common/enums/EWorkerImage';
 
 @injectable()
 export class WorkerService {
-  constructor() {}
+  private docker: Docker;
+
+  constructor() {
+    this.docker = new Docker({ socketPath: '/var/run/docker.sock' });
+  }
+
+  public async existsWorkerById(workerId: string): Promise<boolean> {
+    try {
+      const container = this.docker.getContainer(workerId);
+      await container.inspect();
+
+      return true;
+    } catch {
+      return false;
+    }
+  }
+
+  public async removeWorkerById(
+    workerId: string,
+    t: TFunction<'translation', undefined>
+  ): Promise<void> {
+    try {
+      const container = this.docker.getContainer(workerId);
+
+      await container.remove({ force: true });
+    } catch {
+      throw new Error(t('worker_removal_failed'));
+    }
+  }
+
+  public async createWorker(
+    t: TFunction<'translation', undefined>,
+    imageName: EWorkerImage,
+    containerName: string
+  ): Promise<string> {
+    const existsWorkerById = await this.existsWorkerById(containerName);
+
+    if (existsWorkerById) {
+      await this.removeWorkerById(containerName, t);
+    }
+
+    const container = await this.docker.createContainer({
+      Image: imageName,
+      name: containerName,
+    });
+
+    await container.start();
+
+    return container.id;
+  }
 }
--- packages/core/useCases/worker/WorkerCreator.useCase.ts
+++ packages/core/useCases/worker/WorkerCreator.useCase.ts
@@ -3,17 +3,49 @@ import { TFunction } from 'i18next';
 import { CreateWorkerResponse } from '@core/schema/worker/createWorker/response.schema';
 import { CreateWorkerRequest } from '@core/schema/worker/createWorker/request.schema';
 import { WorkerService } from '@core/services/worker.service';
+import { v4 as uuidv4 } from 'uuid';
+import { EWorkerType } from '@core/common/enums/EWorkerType';
+import { EWorkerImage } from '@core/common/enums/EWorkerImage';
 
 @injectable()
 export class WorkerCreatorUseCase {
   constructor(private readonly workerService: WorkerService) {}
 
+  private getImageWorker(workerType: EWorkerType) {
+    if (workerType === EWorkerType.whatsapp) {
+      return EWorkerImage.whatsapp;
+    }
+
+    if (workerType === EWorkerType.telegram) {
+      return EWorkerImage.telegram;
+    }
+
+    if (workerType === EWorkerType.discord) {
+      return EWorkerImage.discord;
+    }
+
+    return EWorkerImage.baileys;
+  }
+
   async execute(
     t: TFunction<'translation', undefined>,
     input: CreateWorkerRequest
   ): Promise<CreateWorkerResponse> {
+    const imageName = this.getImageWorker(input.worker_type as EWorkerType);
+    const containerName = uuidv4();
+
+    const createWorker = await this.workerService.createWorker(
+      t,
+      imageName,
+      containerName
+    );
+
+    if (!createWorker) {
+      throw new Error(t('worker_creation_failed'));
+    }
+
     return {
-      worker_id: 'teste',
+      worker_id: containerName,
     };
   }
 }
