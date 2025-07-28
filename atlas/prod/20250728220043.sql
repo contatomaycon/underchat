db2344a945124e347e5913a374cec5e5378ab060
Maycon Douglas <contatomaycon@gmail.com>
2025-07-28 19:04:51 -0300

    feat: adicionar configuração de usuário no serviço under-atlas e inserir novos status de setor

---
+++ atlas/prod/20250728220043.sql
@@ -0,0 +1,2 @@
+INSERT INTO "sector_status" ("sector_status_id", "name") VALUES ('449b51fc-e057-4053-baaf-1de5333980b8', 'active');
+INSERT INTO "sector_status" ("sector_status_id", "name") VALUES ('6fa61f81-b90b-454f-a4fd-e0b5700d4dfe', 'inactive');
