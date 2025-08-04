74189da98577e7c2362d12c83ce837f5e820103a
Maycon Douglas <contatomaycon@gmail.com>
2025-08-03 21:11:40 -0300

    feat: remover arquivos de consumo de trabalhador para simplificação do código

--- apps/balance_api/src/consumer/workerDelete.consume.ts
+++
@@ -1,8 +0,0 @@
-import { container } from 'tsyringe';
-import { WorkerDeleterConsume } from '@core/consumer/worker/WorkerDeleter.consume';
-
-export default async function workerDeleteConsume() {
-  const workerDeleterConsume = container.resolve(WorkerDeleterConsume);
-
-  await workerDeleterConsume.execute();
-}
--- apps/balance_api/src/consumer/workerRecreate.consume.ts
+++
@@ -1,8 +0,0 @@
-import { container } from 'tsyringe';
-import { WorkerRecreateConsume } from '@core/consumer/worker/WorkerRecreate.consume';
-
-export default async function workerRecreateConsume() {
-  const workerRecreateConsume = container.resolve(WorkerRecreateConsume);
-
-  await workerRecreateConsume.execute();
-}
