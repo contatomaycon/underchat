62526a3277dc7862d4f87bb1b2fb7419aa3a2dab
Maycon Douglas <contatomaycon@gmail.com>
2025-08-03 23:53:53 -0300

    feat: remover registro de consumidores do KafkaService

--- packages/core/services/kafka.service.ts
+++ packages/core/services/kafka.service.ts
@@ -1,10 +1,9 @@
 import { injectable, inject } from 'tsyringe';
-import { Admin, Consumer, Kafka } from 'kafkajs';
+import { Admin, Kafka } from 'kafkajs';
 
 @injectable()
 export class KafkaService {
   private readonly admin: Admin;
-  private readonly consumers = new Map<string, Consumer>();
 
   constructor(@inject('Kafka') private readonly kafka: Kafka) {
     this.admin = this.kafka.admin();
@@ -35,26 +34,7 @@ export class KafkaService {
     await this.close();
   }
 
-  async registerConsumer(
-    groupId: string,
-    topics: string[],
-    fromBeginning = true
-  ): Promise<void> {
-    const consumer = this.kafka.consumer({ groupId });
-    await consumer.connect();
-
-    for (const topic of topics) {
-      await consumer.subscribe({ topic, fromBeginning });
-    }
-
-    this.consumers.set(groupId, consumer);
-  }
-
   async close(): Promise<void> {
     await this.admin.disconnect();
-
-    for (const consumer of this.consumers.values()) {
-      await consumer.disconnect();
-    }
   }
 }
