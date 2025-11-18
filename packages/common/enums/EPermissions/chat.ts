a01c47cfdc0db22fb2d4847b391afbe86a0bdfbf
Maycon <contatomaycon@gmail.com>
2025-11-18 12:08:44 -0300

    feat: add new chat permissions to EChatPermissions enum, enhancing chat functionality with view_others_chats and pick_queue_chat options

--- packages/common/enums/EPermissions/chat.ts
+++ packages/common/enums/EPermissions/chat.ts
@@ -2,4 +2,6 @@ export enum EChatPermissions {
   chat_group = 'chat_group',
   chat_access = 'chat_access',
   preview_chat = 'preview_chat',
+  view_others_chats = 'view_others_chats',
+  pick_queue_chat = 'pick_queue_chat',
 }
