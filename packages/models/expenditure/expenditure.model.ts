ec47961cc4adcc35c07b7b704d6b7fc7a09edb03
Braian Silva <braian.diogenes@gmail.com>
2025-11-23 23:20:55 -0300

    feat: add expenditure table and model to the database schema, enabling tracking of expenditures with relevant fields

---
+++ packages/models/expenditure/expenditure.model.ts
@@ -0,0 +1,28 @@
+import {
+  pgTable,
+  uuid,
+  timestamp,
+  varchar,
+  text,
+  numeric,
+} from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm';
+
+export const expenditure = pgTable('expenditure', {
+  expenditure_id: uuid().primaryKey().notNull(),
+  name: varchar({ length: 200 }).notNull(),
+  description: text(),
+  price: numeric({ precision: 10, scale: 2 }).notNull(),
+  created_at: timestamp({
+    mode: 'string',
+    withTimezone: true,
+  }).defaultNow(),
+  updated_at: timestamp({
+    mode: 'string',
+    withTimezone: true,
+  }).defaultNow(),
+  deleted_at: timestamp({ mode: 'string', withTimezone: true }),
+});
+
+export const expenditureRelations = relations(expenditure, ({}) => ({}));
+
