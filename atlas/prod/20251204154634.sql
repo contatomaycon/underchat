14aabdf3b20024697265cee8cafe72fa086c2b81
Maycon <contatomaycon@gmail.com>
2025-12-04 15:47:14 -0300

    chore: update atlas.sum file with new hash for 20251204154634.sql and modify existing hash for 20250726164040.sql

---
+++ atlas/prod/20251204154634.sql
@@ -0,0 +1,28 @@
+-- Insert seed "plan_account" table
+INSERT INTO "plan_account" (
+  "plan_account_id",
+  "account_id",
+  "plan_id",
+  "plan_account_status_id",
+  "recurring_payment",
+  "billing_period_id",
+  "last_payment_date",
+  "next_payment_date",
+  "cancellation_date",
+  "created_at",
+  "updated_at"
+) VALUES (
+  'a8c35223-ba8a-45ae-a67c-c9d924a9a2b3',
+  '019a930d-c6f4-75ad-88ff-8d2fcd5839e1',
+  '019a930d-c6f4-75ad-88ff-847edc5f724c',
+  '019a930d-c6f4-75ad-88ff-9a1b2c3d4e5a',
+  true,
+  '019a930d-c6f4-75ad-88ff-9b2c3d4e5f6c',
+  '2025-01-01 15:43:26.672000 +00:00',
+  '2100-01-01 15:43:34.917000 +00:00',
+  NULL,
+  '2025-12-04 18:43:48.412140 +00:00',
+  '2025-12-04 18:43:48.412140 +00:00'
+)
+ON CONFLICT ("plan_account_id") DO NOTHING;
+
