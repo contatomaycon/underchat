88dfa21207e2394156a5d97d47e6bcf66324d4b6
Maycon <contatomaycon@gmail.com>
2025-12-04 13:12:19 -0300

    feat: add detailed plan items for various subscription tiers, including free, bronze, silver, gold, and diamond options, to enhance user understanding of offerings

--- atlas/prod/20250726164649.sql
+++ atlas/prod/20250726164649.sql
@@ -8,4 +8,27 @@ INSERT INTO "plan" ("plan_id", "name", "price", "price_old", "description", "ann
 
 INSERT INTO "plan" ("plan_id", "name", "price", "price_old", "description", "annual_discount", "icon") VALUES ('019a930d-c6f4-75ad-88ff-9a1b2c3d4e5f', 'Teste', 0.00, 0.00, 'Plano gratuito ideal para usuários que desejam conhecer e testar todas as funcionalidades do sistema antes de se comprometerem com um plano pago. Inclui acesso completo a todas as ferramentas e recursos disponíveis, permitindo uma avaliação completa da plataforma. Perfeito para explorar as capacidades de mensageria, chatbot, automações e demais funcionalidades sem custos.', 0, 'tabler-test-pipe');
 
-INSERT INTO "plan_items" ("plan_item_id", "plan_product_id", "plan_id", "quantity") VALUES ('019a930d-c6f4-75ad-88ff-8918f7b17ddc', '019a930d-c6f4-75ad-88ff-789aa55255f8', '019a930d-c6f4-75ad-88ff-847edc5f724c', 5);
+-- Teste: 1x Canais, 1x Cargos, 1x Usuários
+INSERT INTO "plan_items" ("plan_item_id", "plan_product_id", "plan_id", "quantity") VALUES ('019a930d-c6f4-75ad-88ff-8918f7b17dd1', '019a930d-c6f4-75ad-88ff-789aa55255f8', '019a930d-c6f4-75ad-88ff-9a1b2c3d4e5f', 1);
+INSERT INTO "plan_items" ("plan_item_id", "plan_product_id", "plan_id", "quantity") VALUES ('019a930d-c6f4-75ad-88ff-8918f7b17dd2', '019a930d-c6f4-75ad-88ff-7f14ca11114c', '019a930d-c6f4-75ad-88ff-9a1b2c3d4e5f', 1);
+INSERT INTO "plan_items" ("plan_item_id", "plan_product_id", "plan_id", "quantity") VALUES ('019a930d-c6f4-75ad-88ff-8918f7b17dd3', '019a930d-c6f4-75ad-88ff-809fbe7cf6d8', '019a930d-c6f4-75ad-88ff-9a1b2c3d4e5f', 1);
+
+-- Bronze: 1x Canais, 1x Cargos, 1x Usuários
+INSERT INTO "plan_items" ("plan_item_id", "plan_product_id", "plan_id", "quantity") VALUES ('019a930d-c6f4-75ad-88ff-8918f7b17dd4', '019a930d-c6f4-75ad-88ff-789aa55255f8', '019ae6ac-e874-719b-8ee7-1f9d8bc667da', 1);
+INSERT INTO "plan_items" ("plan_item_id", "plan_product_id", "plan_id", "quantity") VALUES ('019a930d-c6f4-75ad-88ff-8918f7b17dd5', '019a930d-c6f4-75ad-88ff-7f14ca11114c', '019ae6ac-e874-719b-8ee7-1f9d8bc667da', 1);
+INSERT INTO "plan_items" ("plan_item_id", "plan_product_id", "plan_id", "quantity") VALUES ('019a930d-c6f4-75ad-88ff-8918f7b17dd6', '019a930d-c6f4-75ad-88ff-809fbe7cf6d8', '019ae6ac-e874-719b-8ee7-1f9d8bc667da', 1);
+
+-- Prata: 1x Canais, 4x Cargos, 4x Usuários
+INSERT INTO "plan_items" ("plan_item_id", "plan_product_id", "plan_id", "quantity") VALUES ('019a930d-c6f4-75ad-88ff-8918f7b17dd7', '019a930d-c6f4-75ad-88ff-789aa55255f8', '019ae6ad-4705-7119-a020-7c26cfc066c4', 1);
+INSERT INTO "plan_items" ("plan_item_id", "plan_product_id", "plan_id", "quantity") VALUES ('019a930d-c6f4-75ad-88ff-8918f7b17dd8', '019a930d-c6f4-75ad-88ff-7f14ca11114c', '019ae6ad-4705-7119-a020-7c26cfc066c4', 4);
+INSERT INTO "plan_items" ("plan_item_id", "plan_product_id", "plan_id", "quantity") VALUES ('019a930d-c6f4-75ad-88ff-8918f7b17dd9', '019a930d-c6f4-75ad-88ff-809fbe7cf6d8', '019ae6ad-4705-7119-a020-7c26cfc066c4', 4);
+
+-- Ouro: 1x Canais, 8x Cargos, 8x Usuários
+INSERT INTO "plan_items" ("plan_item_id", "plan_product_id", "plan_id", "quantity") VALUES ('019a930d-c6f4-75ad-88ff-8918f7b17dda', '019a930d-c6f4-75ad-88ff-789aa55255f8', '019ae6ad-6aea-71ef-9a3a-40e3a7ffa2e3', 1);
+INSERT INTO "plan_items" ("plan_item_id", "plan_product_id", "plan_id", "quantity") VALUES ('019a930d-c6f4-75ad-88ff-8918f7b17ddb', '019a930d-c6f4-75ad-88ff-7f14ca11114c', '019ae6ad-6aea-71ef-9a3a-40e3a7ffa2e3', 8);
+INSERT INTO "plan_items" ("plan_item_id", "plan_product_id", "plan_id", "quantity") VALUES ('019a930d-c6f4-75ad-88ff-8918f7b17ddc', '019a930d-c6f4-75ad-88ff-809fbe7cf6d8', '019ae6ad-6aea-71ef-9a3a-40e3a7ffa2e3', 8);
+
+-- Diamante: 2x Canais, 20x Cargos, 20x Usuários
+INSERT INTO "plan_items" ("plan_item_id", "plan_product_id", "plan_id", "quantity") VALUES ('019a930d-c6f4-75ad-88ff-8918f7b17ddd', '019a930d-c6f4-75ad-88ff-789aa55255f8', '019a930d-c6f4-75ad-88ff-847edc5f724c', 2);
+INSERT INTO "plan_items" ("plan_item_id", "plan_product_id", "plan_id", "quantity") VALUES ('019a930d-c6f4-75ad-88ff-8918f7b17dde', '019a930d-c6f4-75ad-88ff-7f14ca11114c', '019a930d-c6f4-75ad-88ff-847edc5f724c', 20);
+INSERT INTO "plan_items" ("plan_item_id", "plan_product_id", "plan_id", "quantity") VALUES ('019a930d-c6f4-75ad-88ff-8918f7b17ddf', '019a930d-c6f4-75ad-88ff-809fbe7cf6d8', '019a930d-c6f4-75ad-88ff-847edc5f724c', 20);
