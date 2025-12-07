f69ddb73d242edc4a6e6c288f10c28202dfdb803
Maycon <contatomaycon@gmail.com>
2025-12-06 23:53:28 -0300

    feat: update account_test table and model to include additional fields for phone and email with corresponding suffixes for enhanced data management

--- atlas/prod/20251206235029.sql
+++ atlas/prod/20251206235029.sql
@@ -2,8 +2,11 @@
 CREATE TABLE "account_test" (
   "account_test_id" uuid NOT NULL,
   "document" character varying(500) NOT NULL,
-  "phone" character varying(50) NOT NULL,
-  "email" character varying(255) NOT NULL,
+  "document_c" character varying(500) NOT NULL,
+  "phone" character varying(500) NOT NULL,
+  "phone_c" character varying(500) NOT NULL,
+  "email" character varying(500) NOT NULL,
+  "email_c" character varying(500) NOT NULL,
   "created_at" timestamptz NULL DEFAULT now(),
   "updated_at" timestamptz NULL DEFAULT now(),
   PRIMARY KEY ("account_test_id")
