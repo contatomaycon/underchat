f8aa0d76fc8f7c775211d9873274e4af552bd9c7
Maycon <contatomaycon@gmail.com>
2025-12-07 14:00:37 -0300

    feat: enhance account creation by adding account info storage and implementing plan cancellation during account deletion, improving account management functionality

--- packages/repositories/account/AccountCreator.repository.ts
+++ packages/repositories/account/AccountCreator.repository.ts
@@ -1,5 +1,5 @@
 import * as schema from '@core/models';
-import { account, planAccount, apiKey, plan } from '@core/models';
+import { account, planAccount, apiKey, plan, accountInfo } from '@core/models';
 import { CreateAccountRequest } from '@core/schema/account/createAccount/request.schema';
 import { NodePgDatabase } from 'drizzle-orm/node-postgres';
 import { inject, injectable } from 'tsyringe';
@@ -8,6 +8,12 @@ import { randomBytes } from 'node:crypto';
 import { EBillingPeriod } from '@core/common/enums/EBillingPeriod';
 import { currentTime } from '@core/common/functions/currentTime';
 import { eq, and, isNull } from 'drizzle-orm';
+import { EContentWidth } from '@core/common/enums/EContentWidth';
+import { EContentLayoutNav } from '@core/common/enums/EContentLayoutNav';
+import { ELanguage } from '@core/common/enums/ELanguage';
+import { ESkin } from '@core/common/enums/ESkin';
+import { ENavbar } from '@core/common/enums/ENavbar';
+import { EFooter } from '@core/common/enums/EFooter';
 
 @injectable()
 export class AccountCreatorRepository {
@@ -109,6 +115,28 @@ export class AccountCreatorRepository {
         account_id: accountId,
         key,
         name: input.name,
+      });
+
+      const accountInfoId = uuidv7();
+      const now = currentTime();
+
+      await tx.insert(accountInfo).values({
+        account_info_id: accountInfoId,
+        account_id: accountId,
+        content_width: EContentWidth.fluid,
+        content_layout_nav: EContentLayoutNav.vertical,
+        default_locale: ELanguage.pt,
+        skin: ESkin.default,
+        navbar: ENavbar.sticky,
+        footer: EFooter.sticky,
+        is_vertical_nav_collapsed: false,
+        is_vertical_nav_semi_dark: true,
+        light_primary_color: '#2865B7',
+        light_secondary_color: '#5098E5',
+        dark_primary_color: '#152642',
+        dark_secondary_color: '#2865B7',
+        created_at: now,
+        updated_at: now,
       });
 
       return accountId;
