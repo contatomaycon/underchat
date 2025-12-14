92e5d21133126b37cb9fa8306b689cd3e3da1c7e
Maycon <contatomaycon@gmail.com>
2025-12-14 13:54:48 -0300

    feat: enhance PDF generation for conversation history by adding language support, improving internationalization and user experience in document output

---
+++ packages/common/functions/createI18nInstance.ts
@@ -0,0 +1,34 @@
+import i18next from 'i18next';
+import { TFunction } from 'i18next';
+import Backend from 'i18next-fs-backend';
+import path from 'path';
+
+export async function createI18nInstance(
+  language: string
+): Promise<TFunction<'translation', undefined>> {
+  const localesPath = path.join(
+    __dirname,
+    '../../plugins/i18next/locales/{{lng}}/translation.json'
+  );
+
+  const instance = i18next.createInstance();
+
+  await instance.use(Backend).init({
+    lng: language,
+    fallbackLng: 'pt',
+    backend: {
+      loadPath: localesPath,
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+    returnNull: false,
+    returnEmptyString: false,
+    returnObjects: false,
+    initImmediate: false,
+  });
+
+  await instance.loadLanguages(language);
+
+  return instance.t;
+}
