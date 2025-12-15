4e83afee6d4075e45f48e7d4e5359ce2be635f66
Maycon <contatomaycon@gmail.com>
2025-12-15 17:18:23 -0300

    fix: add optional chaining for remote_jid and log results in NotificationMessageSendConsume

--- packages/consumer/notification/NotificationMessageSend.consume.ts
+++ packages/consumer/notification/NotificationMessageSend.consume.ts
@@ -115,7 +115,9 @@ export class NotificationMessageSendConsume {
   ): Promise<void> {
     if (!data.message_whatsapp) return;
 
-    if (data.message_key.remote_jid) {
+    console.log('data.message_key', data.message_key);
+
+    if (data.message_key?.remote_jid) {
       await this.baileysMessageTextService.sendText(
         data.message_key.remote_jid,
         data.message_whatsapp
@@ -128,6 +130,8 @@ export class NotificationMessageSendConsume {
       data.message_key.phone_ddi,
       data.message_key.phone_number
     );
+
+    console.log('result', result);
 
     if (!result.valid || !result.jid) return;
 
