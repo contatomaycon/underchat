e70d4ff16b6453e15fc59a27c64c8ebc581a8e9b
Maycon <contatomaycon@gmail.com>
2025-12-25 17:16:24 -0300

    refactor: remove unused test file 'teste.ts' to clean up the service_api directory

--- apps/service_api/src/teste.ts
+++
@@ -1,7 +0,0 @@
-var a = 1;
-var b = 2;
-var c = [a, b];
-
-c.forEach((item) => {
-  console.log(item);
-});
---
+++ apps/service_api/src/teste2.ts
@@ -0,0 +1,7 @@
+var ab = 1;
+var bc = 2;
+var abc = [ab, bc];
+
+abc.forEach((item2) => {
+  console.log(item2);
+});
