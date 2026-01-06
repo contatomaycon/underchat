43644595a5e267b3726899518ab6e152b894d86d
Maycon Dougkas <contatomaycon@gmail.com>
2026-01-06 15:55:09 -0300

    fix: update seed value for ai_agent_type table from "Others" to "Outros" for localization consistency

--- atlas/prod/20260106140000.sql
+++ atlas/prod/20260106140000.sql
@@ -1,4 +1,4 @@
 -- Insert "Others" seed in "ai_agent_type" table
 INSERT INTO "ai_agent_type" ("ai_agent_type_id", "name", "created_at", "updated_at") VALUES
-  ('019b940d-8790-7a1e-9c5d-8f2e3a4b5c6d', 'Others', NOW(), NOW())
+  ('019b940d-8790-7a1e-9c5d-8f2e3a4b5c6d', 'Outros', NOW(), NOW())
 ON CONFLICT ("ai_agent_type_id") DO NOTHING;
