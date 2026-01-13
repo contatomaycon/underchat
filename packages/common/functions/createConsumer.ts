f9e2cb2852859ebb6e189b411889173e2bdcb4dd
Maycon Dougkas <contatomaycon@gmail.com>
2026-01-13 18:54:17 -0300

    refactor: simplify Kafka consumer creation by removing UUID appending to groupId

--- packages/common/functions/createConsumer.ts
+++ packages/common/functions/createConsumer.ts
@@ -1,12 +1,9 @@
 import { KafkaConsumer } from 'node-rdkafka';
 import { KafkaClient } from '@core/plugins/kafkaStreams';
-import { randomUUID } from 'node:crypto';
 
 export function createConsumer(
   kafka: KafkaClient,
   groupId: string
 ): KafkaConsumer {
-  const uniqueGroupId = `${groupId}-${randomUUID()}`;
-
-  return kafka.createConsumer(uniqueGroupId);
+  return kafka.createConsumer(groupId);
 }
