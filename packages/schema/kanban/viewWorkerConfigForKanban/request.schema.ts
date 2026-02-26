786a98c763ff943652339699338494b80b49252c
Maycon Dougkas <contatomaycon@gmail.com>
2026-02-26 19:52:35 -0300

    refactor: remove viewWorkerConfigForKanban request and response schemas, streamlining schema management in the kanban package

--- packages/schema/kanban/viewWorkerConfigForKanban/request.schema.ts
+++
@@ -1,9 +0,0 @@
-import { Static, Type } from '@sinclair/typebox';
-
-export const viewWorkerConfigForKanbanParamsSchema = Type.Object({
-  worker_id: Type.String(),
-});
-
-export type ViewWorkerConfigForKanbanParams = Static<
-  typeof viewWorkerConfigForKanbanParamsSchema
->;
