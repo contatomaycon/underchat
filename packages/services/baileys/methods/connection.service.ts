729a9f45abccc80f35c549338ffeb4e69d19c03f
Maycon Dougkas <contatomaycon@gmail.com>
2026-04-24 19:18:16 -0300

    fix: atualizar URL da dependência @whiskeysockets/baileys no package.json e pnpm-lock.yaml

--- packages/services/baileys/methods/connection.service.ts
+++ packages/services/baileys/methods/connection.service.ts
@@ -663,7 +663,7 @@ export class BaileysConnectionService {
     const socket = makeWASocket({
       auth: state,
       version,
-      browser: Browsers.android('14'),
+      browser: Browsers.macOS('Desktop'),
       logger: P({ level: 'silent' }),
       printQRInTerminal: false,
       getMessage: async (key) =>
@@ -675,7 +675,6 @@ export class BaileysConnectionService {
       keepAliveIntervalMs: 15_000,
       defaultQueryTimeoutMs: 60_000,
       maxMsgRetryCount: 10,
-      ignoreStatusBroadcast: true,
       ...(proxyAgent
         ? {
             agent: proxyAgent,
