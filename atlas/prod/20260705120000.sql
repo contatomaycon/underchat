6022d31dc10b859470bbe1b52d413f967acc17b5
Maycon Dougkas <contatomaycon@gmail.com>
2026-07-05 20:02:06 -0300

    fix: corrigir a sintaxe de UUID na atualização do status do trabalhador

--- atlas/prod/20260705120000.sql
+++ atlas/prod/20260705120000.sql
@@ -12,8 +12,8 @@ SET
         "worker_whatsapp_official_connection"."worker_id" = "worker"."worker_id"
         AND "worker_whatsapp_official_connection"."deleted_at" IS NULL
     )
-    THEN '019a930d-c6f6-766d-9c84-30af6ecc33b2'
-    ELSE '019a930d-c6f6-766d-9c84-3696c2cd5ed8'
+    THEN '019a930d-c6f6-766d-9c84-30af6ecc33b2'::uuid
+    ELSE '019a930d-c6f6-766d-9c84-3696c2cd5ed8'::uuid
   END,
   "updated_at" = now()
 WHERE "worker_type_id" = '019a930d-c6f6-766d-9c84-55fe10d25e2c';
